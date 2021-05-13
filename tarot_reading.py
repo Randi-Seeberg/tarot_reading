50e55ab9788ccffe944f0302e5a1cf1dc77b0480
Randi Seeberg <randi@seebergart.dk>
2021-05-13 22:09:45 +0200

    List of focus areas

--- tarot_reading.py
+++ tarot_reading.py
@@ -1,1 +1,7 @@
-print("Welcome to Mme Nugget's tarot reading!")
+focus_areas = ["Relationships", "Career", "Personal development"]
+
+def welcome():
+    print("Welcome to Mme Nugget's tarot reading!")
+    print("Your reading will focus on one of three important areas of your life: ")
+    for fa in focus_areas:
+        print fa
