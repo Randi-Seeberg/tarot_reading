6205151f59877e5694a7111cd8d5913e3df460e0
Randi Seeberg <randi@seebergart.dk>
2021-05-14 09:36:48 +0200

    focus_areas dictionary and focus_area print

--- tarot_reading.py
+++ tarot_reading.py
@@ -1,7 +1,21 @@
-focus_areas=["Relationships", "Career", "Personal development"]
+focus_areas={1:"Relationships", 2:"Career", 3:"Personal development"}
+
+#Prints dict focus_areas [number:fa]
+def print_focus_areas():
+    print("\n")
+    for key in focus_areas:
+        print(str(key) +"   "+ focus_areas[key]+"\n")
+
+def choose_focus_area():
+    input=("Which area would you like to focus on? Type R for Relationships, C for Career, or P for Personal development.")
+    return input
+    #So that this can take number and retrieve fa
 
 def welcome():
-    print("Welcome to Mme Nugget's tarot reading!")
-    print("Your reading will focus on one of three important areas of your life: ")
-    for fa in focus_areas:
-        print(fa)
+    print("\nWelcome to Mme Nugget's tarot reading!")
+    print("\nYour reading will focus on one of three important areas of your life: \n")
+    print_focus_areas()
+    choose_focus_area()
+    
+
+print_focus_areas()
