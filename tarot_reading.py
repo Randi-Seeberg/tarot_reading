35933e427c54c5d5653b54d8bd1d60dab287d5f2
Randi Seeberg <randi@seebergart.dk>
2021-05-14 23:15:35 +0200

    optimized choose question

--- tarot_reading.py
+++ tarot_reading.py
@@ -1,26 +1,28 @@
 from questions import questions, add_question
 from random import random
 
+def welcome():
+    print("\nWelcome to Mme Nugget's tarot reading!")
+
 #Prints dict questions [number:q]
 def print_questions():
-    print("\n")
     for key in questions:
         print(str(key) +"   "+ questions[key]+"\n")
 
 def choose_question():
-    choice=input("Please type the number of your chosen question.")
-    if input not in range(len(questions)):
-        print("This is not a valid number. Please try again")
-        choose_question()
-    return choice
-    
-def welcome():
-    print("\nWelcome to Mme Nugget's tarot reading!")
+    choice=int(input("Please type the number of your chosen question: "))
+    if choice in questions.keys():
+        return(choice)
+    else:
+        if choice not in questions.keys():
+            print("This is not a valid number. Please try again.")
+            choose_question()    
     
 def reading():
-    print("\nYour reading will centre on one of {} important questions about your life right now: \n".format())
-    print_focus_areas()
+    print("\nYour reading will centre on one of {} important questions about your life right now: \n".format(len(questions)))
+    print_questions()
     choice=choose_question()
-    print("Interesting choice! Now your digital destiny plugin will shuffle the cards and .")    
+    print("\nYou have chosen the question: " + questions[choice])
+    print("Interesting choice! Now let me shuffle the deck and pick a card for your reading.\n")    
 
-print(range(len(questions)))
+reading()
