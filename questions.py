582ff6a17fbf0062133e279cd2c0b982f9b3438b
Randi Seeberg <randi@seebergart.dk>
2021-05-16 10:04:19 +0200

    Final version

--- questions.py
+++ questions.py
@@ -1,6 +1,10 @@
-questions={1:"Question1", 2:"Question2", 3:"Question3"}
+questions={
+1:"What will be the best career moves for me to take in the near future?", 
+2:"What steps can I take to improve the quality of my relationships with other people?", 
+3:"What would you advise me to focus on in my efforts for personal development?"
+}
 
 def add_question(new_value):
     key = len(questions) + 1
     questions[key] = new_value
-    return questions
+    print(questions)
--- tarot_reading.py
+++ tarot_reading.py
@@ -1,4 +1,4 @@
-from questions import questions, add_question
+from questions import questions
 from cards import cards
 import random
 
@@ -10,53 +10,59 @@ def goodbye():
     print("\nThank you for visiting Mme Nugget's Tarot Reading. I hope to see you again some other time!")
     return
 
-#Prints dict questions [number:q]
 def show_questions():
+    print("\nYour reading will centre on one of {} important questions about your life right now: \n".format(len(questions)))
     for key in questions:
         print(str(key) +"   "+ questions[key]+"\n")
+    return
 
 def choose_question():
-    print("\nYour reading will centre on one of {} important questions about your life right now: \n".format(len(questions)))
-    show_questions()
-    question_choice=int(input("Please type the number of your chosen question: "))
+    choice = input("Please type the number of your chosen question: ")
+    try:
+        question_choice=int(choice)
+    except:
+        print("This is not a valid number. Please try again.")
+        return choose_question()
+
     if question_choice in questions.keys():
         print("\nYou have chosen the question: " + questions[question_choice])
         print("\nInteresting choice!")    
-        return(question_choice)
+        return question_choice
     else:
-        if question_choice not in questions.keys():
-            print("This is not a valid number. Please try again.")
-            choose_question()  
-
+        print("This is not a valid number. Please try again.")
+        return choose_question()
+    
 def pick_random_card():
-    print("\nNow, let me shuffle the cards for you.")
-    print("\nThe cards are shuffled. I will now turn the top card of the deck for your reading.")
+    print("\nLet me shuffle the cards for you.")
+    print("\nI will now turn the top card of the deck for your reading.")
     random_card = random.choice(list(cards.items()))
     print("\nYour card is: " + random_card[0])
     return random_card
 
 def print_reading(question, card):
-    print("\nThis card has much to say in relation to your question: " + questions[question])
-    print("\n" + card[1][question - 1])
+    print("\nThis card has much to say in relation to your question: " + questions[int(question)])
+    print("\n" + card[1][int(question) - 1])
 
 def try_again():
     retry = input("\nWould you like to try another reading? Please type 'y' for yes or 'n' for no: ")
     if retry == "y":
-        return reading()
-    if retry == "n":
-        return
+        reading()
+    elif retry == "n":
+        pass
     else:
-        if retry != "y" and "n":
-            print("\nThis is not a valid input. Please try again.")
-            try_again()
+        print("\nThis is not a valid input. Please try again.")
+        try_again()
 
 def reading():
-    welcome()
+    show_questions()
     question_choice = choose_question()    
     random_card = pick_random_card()
     print_reading(question_choice, random_card)
     try_again()
+    
+def tarot_reading():
+    welcome()
+    reading()
     goodbye()
 
-reading()
-
+tarot_reading()
